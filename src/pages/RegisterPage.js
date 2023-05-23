eeaeddc841b4be3d911d1455556d5a45c12c3e0c
Alageshwaran04 <134389764+Alageshwaran04@users.noreply.github.com>
2023-05-23 19:26:25 +0530

    Add files via upload

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,149 @@
+import { useState } from 'react'
+import './RegisterPage.css'
+import { RegisterApi } from '../services/Api';
+import { storeUserData } from '../services/storage';
+import { isAuthenticated } from '../services/Auth';
+import { Link, Navigate } from 'react-router-dom';
+export default function RegisterPage(){
+
+          const initialStateErrors={
+            email:{required:false},
+            password:{required:false},
+            name:{required:false},
+            custom_error:null
+          };
+
+    const[errors,setErrors]=useState(initialStateErrors);    /* initial error required stop code bottom input box*/
+       /* email:{required:false},
+        password:{required:false},
+        name:{required:false},
+        custom_error:null
+    });*/
+
+        const[loading,setLoading]=useState(false);      /* initial stage of form  */
+
+        const handleSubmit=(event)=>{       /* if register click trigger  */
+            event.preventDefault();        /*   to stop browser loading*/
+            let errors=initialStateErrors;   /* if any input is blank it shows error */
+            let hasError=false;
+            if (inputs.name == ""){
+              errors.name.required=true;
+              hasError=true;
+            }
+            if (inputs.email == ""){
+              errors.email.required=true;
+              hasError=true;
+            }
+            if (inputs.password == ""){
+              errors.password.required=true;
+              hasError=true;
+            }
+            
+            if(!hasError){   /*haserror!=true  */
+              setLoading(true)  /* if has no error once submit loading spinner run */
+               RegisterApi(inputs).then((response)=>{
+                storeUserData(response.data.idToken);
+                
+       
+               }).catch((err)=>{
+                  if(err.response.data.error.message=="EMAIL_EXISTS"){  /*  error handling of email exits*/
+                      setErrors({...errors,custom_error:"Already this email has been generated!"})
+                  } else if(String(err.response.data.error.message).includes('WEAK_PASSWORD')){
+                    setErrors({...errors,custom_error:"password should be atleast 6 characters"})
+                  }
+               }).finally(()=>{
+                setLoading(false) 
+               })
+            }
+
+                      
+            setErrors({...errors});
+            
+        }
+        const[inputs,setInputs]=useState({   /* inital input */
+          email:"",
+          password:"",
+          name:""
+        })
+
+        const handleInput=(event)=>{        /*  handleinput store name=ala,mail=@gmail input value*/
+             setInputs({...inputs,[event.target.name]:event.target.value}) 
+        }
+       if (isAuthenticated()) {             /* if user succesfully login go to next page*/  
+        return <Navigate to="/dashboard" />
+      
+       }
+
+
+    return(
+        <section className="register-block">
+              <div className="container">
+               <div className="row ">
+                <div className="col register-sec">
+                     <h2 className="text-center">Register Now</h2>
+                     <form onSubmit={handleSubmit} className="register-form" action="" >
+                      <div className="form-group">
+                        <label htmlFor="exampleInputEmail1" className="text-uppercase">Name</label><br></br>
+          
+                        <input type="text" className="form-control" onChange={handleInput} name="name" id="" />
+                  
+                        {errors.name.required==true?
+                        (<span className="text-danger" >
+                            Name is required.
+                        </span>):null
+                        }
+                     </div>
+                      <div className="form-group">
+                        <label htmlFor="exampleInputEmail1" className="text-uppercase">Email</label><br></br>
+          
+                        <input type="text"  className="form-control" onChange={handleInput} name="email" id=""  />
+                        {errors.email.required==true?
+                        (<span className="text-danger" >
+                            Email is required.
+                        </span>):null
+                        }
+                     </div>
+                     <div className="form-group">
+                        <label htmlFor="exampleInputPassword1" className="text-uppercase">Password</label> <br></br>
+                        <input  className="form-control" type="password" onChange={handleInput} name="password" id=""/>
+                        {errors.password.required==true?
+                        (<span className="text-danger" >
+                            Password is required.
+                        </span>):null
+                        }
+                     </div>
+                     <div className="form-group">
+                         
+                        <span className="text-danger" >
+                        {errors.custom_error?
+                           (<p>{errors.custom_error}</p>)
+                           :null
+                           }
+                        </span>
+                        { loading?
+                        (<div  className="text-center">
+                          <div className="spinner-border text-primary " role="status">
+                            <span className="sr-only">Loading...</span>
+                          </div>
+                        </div>):null
+                         }
+                        <input type="submit" className="btn btn-login float-right" disabled={loading} value="Register"/>
+                     </div>
+                     <div className="clearfix"></div>
+                     <div className="form-group">
+                       Already have account ? Please <Link to="/login">Login</Link>
+                     </div>
+          
+          
+                     </form>
+          
+          
+                  </div>
+          
+               </div>
+          
+          
+            </div>
+          </section>
+    )
+  }
